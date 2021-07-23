c26c30383e39f29887902a6a75bd3c7d5a281f81
Martin <webmaster@podolak.net>
2021-07-23 11:31:56 +0000

    Delete kb-deadkeys.js

--- kb-deadkeys.js
+++
@@ -1,6 +0,0 @@
-// The original file has moved. This file serves only as a redirection to the new file.
-// The new address of the file is specified below. Please update your bookmark(let)s.
-
-var imported = document.createElement('script');
-imported.src = 'https://languagebookmarklet.de/keyboard/kb.js';
-document.head.appendChild(imported);
